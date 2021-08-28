3b67e5400347cb62bbdb38dc3ba1922ffa31ef15
BNT <BNT@DESKTOP-8C5KO2Q>
2021-08-28 11:01:17 +0700

    fix popup header logo

--- src/components/Common/Header/HeaderUser/index.jsx
+++ src/components/Common/Header/HeaderUser/index.jsx
@@ -1,54 +1,44 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Dropdown, Menu } from 'antd';
+import { Button, Popover } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import userlogo from '../../assets/images/user.png';
 function HeaderUser(props) {
-    //const token = JSON.parse(localStorage.getItem("token"));
     const user = useSelector(state => state.User);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const menu = (
-        (user === null) ? <Menu>
-            <Menu.Item key="0">
-                <a className="header__right-account-item">Chào mừng đến với iBoard </a>
-            </Menu.Item>
-            <Menu.Item key="1">
-                <Link className="header__right-account-item" to="/login">
-                    Đăng Nhập </Link>
-            </Menu.Item>
-        </Menu> : <Menu>
-            <Menu.Item key="0">
+    const content = (
+        (user === null) ?
+            <div>
+                <p>
+                    <Link className="header__right-account-item" to="/login">
+                        Đăng Nhập </Link>
+                </p>
+
+            </div>
+            : <div>
                 <Link to='/thong-tin-khach-hang' className="header__right-account-item">Thông tin tài khoản </Link>
-            </Menu.Item>
-            <Menu.Item key="1">
                 <Link to='/khach-hang/chung-khoan-hien-co' className="header__right-account-item">Thông tin sao kê </Link>
-            </Menu.Item>
-            <Menu.Item key="2">
                 <Link className="header__right-account-item" to="/logout">
                     Đăng xuất </Link>
-            </Menu.Item>
-        </Menu>
-
+            </div>
     );
     return (
         <>
-
-            <Dropdown overlay={menu} placement="bottomRight" arrow>
-                <a className="ant-dropdown-link header__right-account-label" onClick={e => e.preventDefault()}>
+            <Popover
+                content={content}
+                title="Chào mừng đến với iBoard "
+                trigger="focus"
+                placement="bottomRight"
+            >
+                <Button className="ant-dropdown-link header__right-account-label">
                     <Avatar
                         style={{
                             backgroundColor: '#87d068'
                         }}
                         icon={<UserOutlined />}
                     />
-                </a>
-
-            </Dropdown>
-
+                </Button>
+            </Popover>
         </>
     );
 }
